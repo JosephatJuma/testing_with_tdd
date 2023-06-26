9f132d5b96033365904c9fe9206460aa2cf952fa
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-26 09:01:26 +0300

    multipy function modified to pass the test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const assert = require("chai").assert;
 const multiply = require("../multiply");
 describe("multiply", function () {
-  it("Mutiply function returns one", function () {
+  it("Mutiply function return the value specified", function () {
     assert.equal(multiply(2, 2), 4);
   });
 });
