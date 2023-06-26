0b6ffae2d571b48a670b2c6545beac4cdafbf3a1
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-26 12:51:08 +0300

    previous tests commented out

--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
 const assert = require("chai").assert;
 const multiply = require("../multiply");
 describe("multiply", function () {
-  it("Mutiply function return the value specified", function () {
+  // it("Mutiply function returns the value specified", function () {
+  //   assert.equal(multiply(1, 1), 1);
+  // }),
+  //   it("Mutiply function returns the value specified", function () {
+  //     assert.equal(multiply(2, 2), 4);
+  //   }),
+  //   it("Mutiply function returns the value specified", function () {
+  //     assert.equal(multiply(3, 3), 9);
+  //   });
+  // it("Mutiply function returns the value specified", function () {
+  //   assert.equal(multiply(4, 4), 16);
+  // });
+  it("Mutiply function returns the value specified", function () {
     assert.equal(multiply(23, 45), 23 * 45);
   });
 });
