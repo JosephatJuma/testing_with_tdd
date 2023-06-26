cf4057b893417974928f3c9ad0fef42e7316302a
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-26 09:04:51 +0300

    passed fifth test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const assert = require("chai").assert;
 const multiply = require("../multiply");
 describe("multiply", function () {
   it("Mutiply function return the value specified", function () {
-    assert.equal(multiply(4, 4), 16);
+    assert.equal(multiply(23, 45), 23 * 45);
   });
 });
