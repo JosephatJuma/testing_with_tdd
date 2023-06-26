e914c41a2e541992d41df68943892eb67480c576
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-26 08:53:48 +0300

    first tet failed

--- multiply.js
+++ multiply.js
@@ -1,4 +1,1 @@
-function multiply(a, b) {
-  return 1;
-}
-module.exports = multiply;
+
---
+++ test/test.js
@@ -0,0 +1,7 @@
+const assert = require("chai").assert;
+
+describe("multiply", function () {
+  it("First test", function () {
+    assert.equal(multiply(1, 1), 1);
+  });
+});
